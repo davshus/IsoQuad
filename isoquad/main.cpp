7c966472291ff01d9be135284cb5cfb5462bc002
David Shustin <David Shustin@DAVID>
2016-08-08 16:02:30 -0400

    Updated credits

--- isoquad/main.cpp
+++ isoquad/main.cpp
@@ -1,1 +1,8 @@
 #pragma warning(disable:4996)
+#include "SerialClass.h"
+#include <stdio.h>
+int main(int argc, char *argv[]) {
+	printf("\n@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n####### ISOTONIC QUADCOPTER #######\n#######    PROGRAMMED BY    #######\n####### DAVID SHUSTIN, 2016 #######\n@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n\n");
+	system("pause");
+	return 0;
+}
